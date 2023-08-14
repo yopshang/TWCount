1a2eb6da2078c19fa23f4137cb0e70fc475aee14
Shang <yopshang@gmail.com>
2023-08-14 19:39:12 +0800

    fix: card layout

--- src/components/CountHall.jsx
+++ src/components/CountHall.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import {useState} from 'react';
 
 export default function CountHall() {
+    const [cards, setCards] = useState([
+        {title: '個人淨值計算機'},
+        {title: '測試計算機1'},
+        {title: '測試計算機1'},
+        {title: '測試計算機1'},
+        {title: '測試計算機1'},
+        {title: '測試計算機1'},
+        {title: '測試計算機1'},
+    ])
   // 元件的內容
-  return (<div class="container">
-    <ul class="d-flex flex-wrap list--no justify-between p-10">
-        <li className="card">個人淨值計算機
-            <div className="marker">
-                <i className="el-icon-arrow-right"></i>
-                <div className="title">個人淨值計算機</div>
-            </div>
-        </li>
-        <li className="card">測試計算機1</li>
-        <li className="card">測試計算機2</li>
-        <li className="card">測試計算機3</li>
-        <li className="card">測試計算機4</li>
-        <li className="card">測試計算機4</li>        <li className="card">測試計算機4</li>
-        <li className="card">測試計算機4</li>        <li className="card">測試計算機4</li>
-        <li className="card">測試計算機4</li>        <li className="card">測試計算機4</li>
-        <li className="card">測試計算機4</li>
-        <li className="card">測試計算機4</li>
+  return (<div className="container">
+    <ul className="card_container d-flex flex-wrap list--no justify-start p-10">
+        {
+            cards.map((card, index)=>{
+                return (<li key={index}  className="card">{card.title}
+                    <div className="marker">
+                        <i className="el-icon-arrow-right"></i>
+                        <div className="title">{card.title}</div>
+                    </div>
+                </li>)
+            })
+        }
     </ul>
   </div>);
 };
