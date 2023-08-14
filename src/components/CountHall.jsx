a57022b484ce9055a1929ee27266924fca1a802a
Shang <yopshang@gmail.com>
2023-08-14 19:05:30 +0800

    feature: base card component

--- src/components/CountHall.jsx
+++ src/components/CountHall.jsx
@@ -1,9 +1,20 @@
-// CountHall.jsx
 import React from 'react';
 
 export default function CountHall() {
   // 元件的內容
-  return <div>這是 CountHall 元件</div>;
+  return (<div class="container">
+    <ul class="d-flex flex-wrap list--no justify-between p-10">
+        <li className="card">個人淨值計算機</li>
+        <li className="card">測試計算機1</li>
+        <li className="card">測試計算機2</li>
+        <li className="card">測試計算機3</li>
+        <li className="card">測試計算機4</li>
+        <li className="card">測試計算機4</li>        <li className="card">測試計算機4</li>
+        <li className="card">測試計算機4</li>        <li className="card">測試計算機4</li>
+        <li className="card">測試計算機4</li>        <li className="card">測試計算機4</li>
+        <li className="card">測試計算機4</li>
+        <li className="card">測試計算機4</li>
+    </ul>
+  </div>);
 };
 
-CountHall;  // 使用默認匯出
